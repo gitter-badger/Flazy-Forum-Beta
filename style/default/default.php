2ef0699d84958e17b3fd6c7179143a87e954f5ef
Martin <support@ancientcraft.eu>
2015-04-30 21:57:18 +0300

    Updated CSS

--- style/default/default.php
+++ style/default/default.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-<link href="" rel="stylesheet" type="text/css" media="screen, projection">
-<?php '<link rel="stylesheet" type="text/css" href="'.$base_url.'/style/'.$forum_user['style'].'/css/print.css" />' ?>
+<link rel="stylesheet" type="text/css" href="/style/default/css/print.css" />
+<link rel="stylesheet" type="text/css" href="/style/default/css/alerts.css" />
