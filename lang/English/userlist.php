a41ad32fb6b9df15ca4318b37363ad9748de5997
Martin <support@ancientcraft.eu>
2015-05-04 18:13:28 +0300

    Updated English Language

--- lang/English/userlist.php
+++ lang/English/userlist.php
@@ -1,42 +1,46 @@
 <?php
+/**
+ * English language pack.
+ * @package Flazy_English
+ */
 
-// Language definitions used in userlist.php
+/** Pack. is used in userlist.php */
 $lang_ul = array(
 
-'Users'					=>	'Users',
+'Users'					=>	'Members',
 'Users found'			=>	'Users found matching your criteria',
 'No users found'		=>	'No users were found matching your criteria',
 'User search head'		=>	'Search for users by username or by predefined criteria',
 'User results all'		=>	'All currently registered users',
 'User results matching'	=>	'Currently registered users matching your criteria',
-/*RUS*/'Group info'			=>	'Введите имя пользователя для поиска и/или укажите группу.',
-/*RUS*/'Wildcard info'			=>	'Имя пользователя может быть пустым. Используйте <strong>*</strong> в качестве шаблона для частичного совпадения.',
-/*RUS*/'Sort info'				=>	'Отсортируйте пользователей по имени, дате регистрации или количеству сообщений и упорядочите по возрастанию/убыванию.',
+'Group info'			=>	'Enter username for search or username group.',
+'Wildcard info'			=>	'Username can not be empty.  Use <strong>*</strong> to search part from username.',
+'Sort info'				=>	'Сортиране потребителите по име, дата на регистриране или броя на мненията, и във възходящ/низходящ ред.',
 'User find legend'		=>	'Find users',
 'Username'				=>	'Username',
-/*RUS*/'Username 2'			=>	'Имени',
-/*'Male'			=>	'Мужчина',
-'Female'		=>	'Женщина',*/
+'Username 2'			=>	'Usernames',
+/*'Male'			=>	'Male',
+'Female'		=>	'Female',*/
 'Search for username'	=>	'Username to search for',
 'Username help'			=>	'May be left blank. Use wildcard character <strong>*</strong> for partial matches.',
 'User group'			=>	'Show users from group',
 'Sort users by'			=>	'Sort user list by',
 'User sort order'		=>	'User list sort order',
 'No of posts'			=>	'Number of posts',
-/*RUS*/'Registered'			=>	'Registration date',
-/*RUS*/'Registered 2'			=>	'Дате регистрации',
-/*RUS*/'Last visit'			=>	'Был',
-/*RUS*/'Last visit 2'			=>	'Последнему визиту',
+'Registered'			=>	'Registered',
+'Registered 2'			=>	'Registered',
+'Last visit'			=>	'Last Login',
+'Last visit 2'			=>	'Last visit',
 'Ascending'				=>	'Ascending',
 'Descending'			=>	'Descending',
 'All users'				=>	'All users',
-/*RUS*/'User list options'		=>	'Настройка списка участников',
 'Perform new search'	=>	'Perform new user search',
+'User list options'		=>	'User list options',
 'Table summary'			=>	'List of users filtered and sorted according to the criteria (if any) you have chosen.',
 'Submit user search'	=>	'Submit user criteria',
 'Title'					=>	'Title',
-/*RUS*/'Reputation'			=>	'Репутация',
-/*RUS*/'Reputation 2'			=>	'Репутации',
+'Reputation'			=>	'Reputation',
+'Reputation 2'			=>	'Reputations',
 'Posts'					=>	'Posts'
 
 );
