4b317f8d74c889ea6a154a0a206e2f26d114156f
Martin <support@ancientcraft.eu>
2015-05-19 21:24:01 +0300

    У

--- cache/cache_hooks.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('FORUM_HOOKS_LOADED', 1);
-
-$hooks = array (
-);
-
-?>
--- cache/cache_report.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('FORUM_REPORT_LOADED', 1);
-
-$forum_report = array(
-'report'	=> '',
-);
-
-?>
